392d5639dc03b6f987e5832d5f606694407a9939
Islam Kodzokov <196956442+zrasute@users.noreply.github.com>
2025-01-31 11:41:05 +0300

    create database config

--- src/hse_football_api/core/config.py
+++ src/hse_football_api/core/config.py
@@ -11,9 +11,19 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    pool_size: int = 50
+    max_overflow: int = 10
+
+
 class Settings(BaseSettings):
     run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
-settings = Settings()
+
+settings = Settings()  # noqa
