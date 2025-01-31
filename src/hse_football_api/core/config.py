b9726edd7ae6d232314dbd054f4125336e335006
Islam Kodzokov <196956442+zrasute@users.noreply.github.com>
2025-01-31 18:01:54 +0300

    add naming convention for models

--- src/hse_football_api/core/config.py
+++ src/hse_football_api/core/config.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-from pydantic import BaseModel, PostgresDsn
+from pydantic import BaseModel, Field, PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,6 +21,18 @@ class DatabaseConfig(BaseModel):
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
+
+    # noinspection PyDataclass
+    naming_convention: dict[str, str] = Field(
+        frozen=True,
+        default={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_N_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        },
+    )
 
 
 class Settings(BaseSettings):
--- src/hse_football_api/core/models/base.py
+++ src/hse_football_api/core/models/base.py
@@ -1,5 +1,8 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase
+
+from hse_football_api.core.config import settings
 
 
 class Base(DeclarativeBase):
-    pass
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
