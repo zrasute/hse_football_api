fc3b69d91dbebd194aabcf7b90a5cff23cda89cb
Islam Kodzokov <196956442+zrasute@users.noreply.github.com>
2025-02-07 13:39:36 +0300

    create player model

--- src/hse_football_api/core/models/__init__.py
+++ src/hse_football_api/core/models/__init__.py
@@ -1,7 +1,9 @@
 __all__ = [
     "db_helper",
     "Base",
+    "Player",
 ]
 
 from .base import Base
 from .db_helper import db_helper
+from .player import Player
---
+++ src/hse_football_api/core/models/player.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+from .base import Base
+from .mixins import IdPkMixin
+
+
+class Player(Base, IdPkMixin):
+    __tablename__ = "players"
+
+    name: Mapped[str] = mapped_column(String(32))
+    last_name: Mapped[str] = mapped_column(String(32))
+    date_of_birth: Mapped[datetime]
+    is_verified: Mapped[bool] = mapped_column(default=False)
