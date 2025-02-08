7998597f698d86ea3bcb015993e9726e33498ccb
Islam Kodzokov <196956442+zrasute@users.noreply.github.com>
2025-02-08 13:10:13 +0300

    add event_time column for match_events table

--- src/hse_football_api/core/models/match_event.py
+++ src/hse_football_api/core/models/match_event.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Enum, ForeignKey
+from sqlalchemy import CheckConstraint, Enum, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
@@ -11,12 +11,17 @@ from .mixins import IdPkMixin
 class MatchEvent(Base, IdPkMixin):
     __tablename__ = "match_events"
 
+    __table_args__ = (
+        CheckConstraint("event_time >= 0 AND event_time <= 60", name="event_time"),
+    )
+
     match_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("matches.id"))
     team_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("teams.id"))
     player_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("players.id"))
     event_type: Mapped[EventType] = mapped_column(
         Enum(EventType, name="event_type_enum")
     )
+    event_time: Mapped[int]
 
     def __repr__(self) -> str:
         return (
